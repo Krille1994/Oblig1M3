147eaf46e08b30168dc0ffa9685098ed6ab4bb4b
Kristian <kristian.risvaer@gmail.com>
2021-03-08 12:41:01 +0100

    Initial commit

---
+++ Oblig1M3/FamilyApp.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oblig1M3
+{
+    public class FamilyApp
+    {
+        public string WelcomeMessage = "Hallo! ";
+        public string CommandPrompt = "Skriv en kommando: ";
+        public List<Person> People;
+        public FamilyApp(params Person[] family)
+        {
+            People = new List<Person>(family);
+        }
+        public string HandleCommand(string input)
+        {
+            if (input == "hjelp")
+            {
+                return helpText();
+            }
+            else if (input == "liste")
+            {
+                return showList(People);
+            }
+            else if (input.Contains("vis "))
+            {
+                //Console.WriteLine(People.Count);
+                return getPersonById(input, People);
+            }
+            else
+            {
+                return "Du kan skrive 'hjelp' for å se hva kommandoene er";
+            }
+        }
+
+        private static string showList(List<Person> People)
+        {
+            string tekst = "";
+            for (int i = 0; i < People.Count; i++)
+            {
+                tekst += People[i].GetDescription() + "\n";
+            }
+            return tekst;
+        }
+
+        private static string getPersonById(string input, List<Person> People)
+        {
+            int ID = Int32.Parse(input.Substring(input.IndexOf(" ") + 1));
+            string tekst = "";
+            List<string> children = new List<string>();
+
+            for (int i = 0; i < People.Count; i++)
+            {
+                if (People[i].Id == ID)
+                {
+                    tekst += People[i].GetDescription();
+                    for (int j = 0; j < People.Count; j++)
+                    {
+                        if (People[j].Father != null)
+                        {
+                            if (People[j].Father.Id == People[i].Id)
+                            {
+                                children.Add(People[j].FirstName + " (Id=" + People[j].Id + ") Født: " + People[j].BirthYear);
+                            }
+                        }
+                        if (People[j].Mother != null)
+                        {
+                            if (People[j].Mother.Id == People[i].Id)
+                            {
+                                children.Add(People[j].FirstName + " (Id=" + People[j].Id + ") Født: " + People[j].BirthYear);
+                            }
+                        }
+                    }
+                }
+            }
+            tekst += "\n";
+            if (children.Count != 0)
+                tekst += "  Barn:\n    ";
+            {
+                for (int i = 0; i < children.Count; i++)
+                {
+                    if (i == children.Count - 1)
+                    {
+                        tekst += children[i] + "\n";
+                    }
+                    else
+                    {
+                    tekst += children[i] + "\n    ";
+                    }
+                }
+            }
+            return tekst;
+        }
+
+        private static string helpText()
+        {
+            return @"Kommandoer:
+
+liste
+
+Du kan skrive liste for å få en liste over alle personer.
+
+vis Id
+
+Du kan skrive denne kommandoen for å skjekke opp en person med en spesifikk id, 
+eks: vis 23";
+        }
+    }
+}
---
+++ Oblig1M3/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oblig1M3
+{
+    public class Person
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public int BirthYear;
+        public int DeathYear;
+        public Person Father;
+        public Person Mother;
+
+        public string GetDescription()
+        {
+            //return string.Format("{0} {1} (Id={2}) Født: {3} Død: {4} Far: {5} (Id={6}) Mor: {7} (Id={8})", FirstName, LastName, Id, BirthYear, DeathYear, Father.FirstName, Father.Id, Mother.FirstName, Mother.Id);
+
+            string fullString = "";
+            if (FirstName != null)
+            {
+                fullString += FirstName + " ";
+            }
+            if (LastName != null)
+            {
+                fullString += LastName + " ";
+            }
+            if (Id != 0)
+            {
+                fullString += "(Id="+Id+") ";
+            }
+            if (BirthYear != 0)
+            {
+                fullString += "Født: "+BirthYear+" ";
+            }
+            if (DeathYear != 0)
+            {
+                fullString += "Død: "+DeathYear+" ";
+            }
+            if (Father != null)
+            {
+                if (Father.FirstName != null)
+                {
+                    fullString += "Far: " + Father.FirstName + " ";
+                }
+                if (Father.Id != 0)
+                {
+                    fullString += "(Id=" + Father.Id + ") ";
+                }
+            }
+            if (Mother != null)
+            {
+                if (Mother.FirstName != null)
+                {
+                    fullString += "Mor: " + Mother.FirstName + " ";
+                }
+                if (Mother.Id != 0)
+                {
+                    fullString += "(Id=" + Mother.Id + ")";
+                }
+            }
+
+            
+            string fullStringTrimmed = fullString.Trim();
+            return fullStringTrimmed;
+
+
+        }
+    }
+}
